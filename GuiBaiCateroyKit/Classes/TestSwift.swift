8046a4f45d90511f01fc572b4314dcb568219b5d
caimingsong <caimingsong@qiyi.com>
2020-04-25 20:22:05 +0800

    添加swift

---
+++ GuiBaiCateroyKit/Classes/TestSwift.swift
@@ -0,0 +1,15 @@
+//
+//  TestSwift.swift
+//  GuiBaiCateroyKit_Example
+//
+//  Created by 蔡明松 on 2020/4/25.
+//  Copyright © 2020 caimingsong. All rights reserved.
+//
+
+import Foundation
+
+open class SwiftMode: NSObject {
+    @objc open class func show() {
+        print("哈哈哈")
+    }
+}
